2fb5119930cfe23112151f615af48c9e3bbb8136
xLeachimx <michael.huelsman@gmail.com>
2015-01-03 19:26:11 -0500

    Added basic class structure

---
+++ lib/genetic_sculpture.rb
@@ -0,0 +1,17 @@
+require_relative 'voxel'
+
+class GeneticSculpture
+	attr_accessor :voxels
+	attr_accessor :size
+
+	def initialize size
+		rGen = Random.new
+		voxels = Array.new(size)
+		for i in 0...size
+			x = rGen.rand(128) + rGen.rand
+			y = rGen.rand(128) + rGen.rand
+			z = rGen.rand(128) + rGen.rand
+			voxels[i] = Voxel.new(x,y,z)
+		end
+	end
+end
