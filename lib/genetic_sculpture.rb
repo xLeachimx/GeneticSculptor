68818546f9cd1ec00b921ceee3deee45de666a7b
xLeachimx <michael.huelsman@gmail.com>
2015-01-04 14:00:00 +0100

    Added mutate function

--- lib/genetic_sculpture.rb
+++ lib/genetic_sculpture.rb
@@ -6,12 +6,24 @@ class GeneticSculpture
 
 	def initialize size
 		rGen = Random.new
-		voxels = Array.new(size)
-		for i in 0...size
+		voxels = Array.new(@size)
+		for i in 0...@size
 			x = rGen.rand(128) + rGen.rand
 			y = rGen.rand(128) + rGen.rand
 			z = rGen.rand(128) + rGen.rand
-			voxels[i] = Voxel.new(x,y,z)
+			@voxels[i] = Voxel.new(x,y,z)
 		end
+	end
+
+	def mutate
+		rGen = Random.new
+		changePoint = rGen.rand(@size)
+		x = rGen.rand(128) + rGen.rand
+		y = rGen.rand(128) + rGen.rand
+		z = rGen.rand(128) + rGen.rand
+		@voxels[changePoint] = Voxel.new(x,y,z)
+	end
+
+	def mate other
 	end
 end
