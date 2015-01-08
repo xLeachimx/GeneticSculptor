95e654e98ce848333b9bcc95284cd45a10f01e2c
xLeachimx <michael.huelsman@gmail.com>
2015-01-08 13:02:47 -0500

    Adding metrics is fun

---
+++ design/bounding_box.md
@@ -0,0 +1,23 @@
+eneticSculpture Class inherits GeneticObject
+===============
+Fields
+===============
+* x
+* y
+* z
+* width
+* height
+* depth
+
+===============
+Methods
+===============
+* inside?
+  * input - a voxel
+  * output - boolean value
+  * description - returns true iff the voxel resides at least partly inside the bounding box
+
+===============
+Notes
+===============
+* Bounding box x,y,z position is for the center of the box
--- lib/genetic_sculpture.rb
+++ lib/genetic_sculpture.rb
@@ -1,8 +1,16 @@
 require_relative 'voxel'
 
+default_metrics = {
+	volume: 0,
+	spaceUse: 0,
+	spread: 0,
+	duplicate: 0,
+}
+
 class GeneticSculpture
 	attr_accessor :voxels
 	attr_accessor :size
+	attr_accessor :metrics
 
 	def initialize size
 		rGen = Random.new
@@ -13,6 +21,7 @@ class GeneticSculpture
 			z = rGen.rand(128) + rGen.rand
 			@voxels[i] = Voxel.new(x,y,z)
 		end
+		@metrics = default_metrics;
 	end
 
 	def mutate
@@ -25,5 +34,103 @@ class GeneticSculpture
 	end
 
 	def mate other
+		rGen = Random.new
+		crossPoint = rGen.rand(@size)
+		one = GeneticSculpture.new(@size)
+		two = GeneticSculpture.new(@size)
+		for i in 0...crossPoint
+			one.voxels[i] = @voxels[i]
+			two.voxels[i] = other.voxels[i]
+		end  
+
+		for i in crossPoint...@size
+			one.voxels[i] = other.voxels[i]
+			two.voxels[i] = @voxels[i]
+		end
+
+		return [one, two]
+	end
+
+	def evalMetrics
+		@metrics[:spread] = spread
+		@metrics[:volume] = height() * width() * depth()
+		@metrics[:spaceUse] = spaceUse
+		@metrics[:duplicate] = duplicates
+	end
+
+	def compareMetrics comp
+	end
+
+	private
+
+	def height
+		minMax = minMaxY
+		return minMax[1] - minMax[0]
+	end
+
+	def width
+		minMax = minMaxX
+		return minMax[1] - minMax[0]
+	end
+
+	def depth
+		minMax = minMaxZ
+		return minMax[1] - minMax[0]
+	end
+
+	def spread
+	end
+
+	def spaceUse
+	end
+
+	def duplicates
+	end
+
+	# Methods for helping to calculate metrics
+
+	def minMaxX
+		highest = @voxels[0].x
+		lowest = @voxels[0].x
+		for v in @voxels
+			if highest < v.x
+				highest = v.x
+			end
+
+			if lowest > v.x
+				lowest = v.x
+			end
+		end
+		return [lowest,highest]
+	end
+
+	def minMaxY
+		highest = @voxels[0].y
+		lowest = @voxels[0].y
+		for v in @voxels
+			if highest < v.y
+				highest = v.y
+			end
+
+			if lowest > v.y
+				lowest = v.y
+			end
+		end
+		return [lowest,highest]
+	end
+
+	def minMaxZ
+		highest = @voxels[0].z
+		lowest = @voxels[0].z
+		for v in @voxels
+			if highest < v.z
+				highest = v.z
+			end
+
+			if lowest > v.z
+				lowest = v.z
+			end
+		end
+		return [lowest,highest]
 	end
 end
